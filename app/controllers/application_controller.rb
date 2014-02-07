a73a0bea181112225ea49119a91b78d4dcd6f6ad
Your Name <devanallen@artistsmecca.com>
2014-02-07 14:38:47 -0700

    addresses issue #44 fixes picture flickering but needs function moved to chat_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   helper_method :render_profile_pic
+  helper_method :check_new_messages
+  
   
   def render_profile_pic (user)
     ActionController::Base.helpers.image_tag("profile_pics/" + user.profile_pic)
   end
+  
+  def check_new_messages
+    @newest_message = Message.last.id.to_i
+    @last_loaded_message = session[:message_id]
+    
+    true if (@newest_message >= @last_loaded_message)     
+  end
+  
 end
